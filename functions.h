e24cea3474b18b420ac065e71ecf042d0107816a
Kabeer Makkar <kabeermakkar28@gmail.com>
2024-11-17 17:17:03 -0500

    test push

---
+++ functions.h
@@ -0,0 +1,37 @@
+// functions.h
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+typedef struct {
+    // add attributes
+} User;
+
+typedef struct {
+    // add attributes
+} Post;
+
+typedef struct {
+    // add attributes
+} Message;
+
+void testingParser(int arg1, char *arg2);
+User* create_user(const char* name, const char* email); // int user_id is auto-generated to be unique
+void add_friend(User* user1, User* user2); // users user1 and user2 are now friends
+void delete_friend(User* user1, User* user2); // users user1 and user2 are no longer friends
+void delete_user(User* user); // user is deleted
+void print_users(); // prints all user names in ascending order
+void change_user_name(User* user, char* new_name);
+void change_user_email(User* user, char* new_email);
+void print_friends(User* user); // prints user's friends in ascending order
+User* search_user_by_name(const char* name);
+User* search_user_by_email(const char* email);
+User** mutual_friends(User* user1, User* user2); // returns an array of pointers to the mutual friends
+void print_mutual_friends(User** friends); // prints mutual friends' user names in acsending order
+Message* create_message(User* sender, User* receiver, const char* content); // int message_id is auto-generated to be unique
+void print_message(Message* message);
+void display_chat(User* user1, User* user2); // print messages in FIFO
+Post* new_post(User* user, const char* content); // post id is auto-generated to be unique
+void add_like(Post* post, User* user); // user is the individual who liked the post
+void display_feed(User* user1);
+
+#endif
---
+++ main.c
@@ -0,0 +1,172 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "functions.h"
+
+#define MAX_LINE_LENGTH 1024
+#define MAX_ARGUMENTS 10
+
+// test push
+
+// Function to call the appropriate function based on the function name
+void callFunction(char *funcName, char **args, int argCount) {
+    
+    if (strcmp(funcName, "testingParser") == 0) {
+        if (argCount >= 2) {
+            testingParser(atoi(args[0]), args[1]);
+        } else {
+            printf("Invalid arguments for function: %s\n", funcName);
+        }
+    } 
+    
+    // UNCOMMENT ME AFTER YOU HAVE WRITTEN MY FUNCTION
+
+    // else if (strcmp(funcName, "create_user") == 0) {
+    //     if (argCount >= 2) {
+    //         create_user(args[0], args[1]);
+    //     } else {
+    //         printf("Invalid arguments for %s\n", funcName);
+    //     }
+    // } 
+
+    // else if (strcmp(funcName, "print_users") == 0) {
+    //     if (argCount >= 0) {
+    //         print_users();
+    //     } else {
+    //         printf("Invalid arguments for %s\n", funcName);
+    //     }
+    // } 
+
+    // else if (strcmp(funcName, "search_user_by_name") == 0) {
+    //     if (argCount >= 1) {
+    //         search_user_by_name(args[0]);
+    //     } else {
+    //         printf("Invalid arguments for %s\n", funcName);
+    //     }
+    // } 
+
+    // else if (strcmp(funcName, "search_user_by_email") == 0) {
+    //     if (argCount >= 1) {
+    //         search_user_by_email(args[0]);
+    //     } else {
+    //         printf("Invalid arguments for %s\n", funcName);
+    //     }
+    // } 
+
+    // else if (strcmp(funcName, "delete_user") == 0) {
+    //     if (argCount >= 1) {
+    //         delete_user(search_user_by_name(args[0]));
+    //     } else {
+    //         printf("Invalid arguments for %s\n", funcName);
+    //     }
+    // } 
+
+    // else if (strcmp(funcName, "change_user_name") == 0) {
+    //     if (argCount >= 2) {
+    //         change_user_name(search_user_by_name(args[0]), args[1]);
+    //     } else {
+    //         printf("Invalid arguments for %s\n", funcName);
+    //     }
+    // } 
+
+    // else if (strcmp(funcName, "change_user_email") == 0) {
+    //     if (argCount >= 2) {
+    //         change_user_email(search_user_by_name(args[0]), args[1]);
+    //     } else {
+    //         printf("Invalid arguments for %s\n", funcName);
+    //     }
+    // } 
+
+    // else if (strcmp(funcName, "add_friend") == 0) {
+    //     if (argCount >= 2) {
+    //         add_friend(search_user_by_name(args[0]), search_user_by_name(args[1]));
+    //     } else {
+    //         printf("Invalid arguments for %s\n", funcName);
+    //     }
+    // } 
+
+    // else if (strcmp(funcName, "delete_friend") == 0) {
+    //     if (argCount >= 2) {
+    //         delete_friend(search_user_by_name(args[0]), search_user_by_name(args[1]));
+    //     } else {
+    //         printf("Invalid arguments for %s\n", funcName);
+    //     }
+    // } 
+
+    // else if (strcmp(funcName, "print_mutual_friends") == 0) {
+    //     if (argCount >= 2) {
+    //         print_mutual_friends(mutual_friends(search_user_by_name(args[0]), search_user_by_name(args[1])));
+    //     } else {
+    //         printf("Invalid arguments for %s\n", funcName);
+    //     }
+    // } 
+
+    // else if (strcmp(funcName, "create_message") == 0) {
+    //     if (argCount >= 3) {
+    //         create_message(search_user_by_name(args[0]), search_user_by_name(args[1]), args[2]);
+    //     } else {
+    //         printf("Invalid arguments for %s\n", funcName);
+    //     }
+    // } 
+
+    // else if (strcmp(funcName, "display_chat") == 0) {
+    //     if (argCount >= 2) {
+    //         display_chat(search_user_by_name(args[0]), search_user_by_name(args[1]));
+    //     } else {
+    //         printf("Invalid arguments for %s\n", funcName);
+    //     }
+    // } 
+
+    // else if (strcmp(funcName, "new_post") == 0) {
+    //     if (argCount >= 2) {
+    //         new_post(search_user_by_name(args[0]), args[1]);
+    //     } else {
+    //         printf("Invalid arguments for %s\n", funcName);
+    //     }
+    // } 
+
+    // else if (strcmp(funcName, "display_feed") == 0) {
+    //     if (argCount >= 1) {
+    //         display_feed(search_user_by_name(args[0]));
+    //     } else {
+    //         printf("Invalid arguments for %s\n", funcName);
+    //     }
+    // } 
+
+    else {
+        printf("Unknown function: %s\n", funcName);
+    }
+}
+
+int main() {
+    FILE *file = fopen("test_cases.csv", "r");
+    if (file == NULL) {
+        perror("Unable to open file");
+        return 1;
+    }
+
+    char line[MAX_LINE_LENGTH];
+    while (fgets(line, sizeof(line), file)) {
+        char *token;
+        char *funcName = NULL;
+        char *args[MAX_ARGUMENTS];
+        int argCount = 0;
+
+        token = strtok(line, ",\n");
+        while (token != NULL) {
+            if (funcName == NULL) {
+                funcName = token;
+            } else {
+                args[argCount++] = token;
+            }
+            token = strtok(NULL, ",\n");
+        }
+
+        if (funcName != NULL) {
+            callFunction(funcName, args, argCount);
+        }
+    }
+
+    fclose(file);
+    return 0;
+}
